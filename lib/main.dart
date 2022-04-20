37f10fd114dff7749f1cf7b48fff8cfe07aba7eb
lorico salim <sparker28032002@gmail.com>
2022-04-20 22:57:01 +0700

    (Push Pertama)

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ) ,
+        backgroundColor: Colors.orange,
+        body: Center(
+          child: Column(
+            children: [
+            Image(
+              image: AssetImage('images/fti.png') 
+            ),
+              Text('Hello World'),
+          ],
+          ),  
+        ),
+      )
+    );
+  }
+}
